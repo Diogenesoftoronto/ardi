6044dccebe673dbae8b160316774c251e24e56e0
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-07-17 14:42:03 -0400

    Read, open and close the mets file

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -27,14 +28,33 @@ Only a single path at a time is allowed`)
 	if err != nil {
 		log.Fatalf("Could not get working directory")
 	}
-	mets_path := filepath.Join(cwd, path, "data", "mets.xml")
-	_ = mets_path
-	file, err := os.Open(mets_path)
+	// Need to look for a file that includes a file with a mets.
+	// The mets will have a uuid: mets.<uuid>.xml
+	dataPath := filepath.Join(cwd, path, "data")
+	cwd = dataPath
+	dir, err := os.ReadDir(dataPath)
 	if err != nil {
 		log.Fatalf("This path does not exist.")
 	}
+	var entry os.DirEntry
+	for _, dEntry := range dir {
+		if strings.Contains(dEntry.Name(), "mets") && !dEntry.IsDir() {
+			entry = dEntry
+			break
+		}
+	}
+	mets, err := os.Open(entry.Name())
+	defer func {
+		err := mets.Close()
+		if err != nil {
+			log.Fatal("Failed to close the mets file.")
+		}
+	}()
+	if err != nil {
+		log.Fatalf("Could not open %s", entry.Name())
+	}
 	// We want to read the file
-	r := tar.NewReader(file)
+	r := tar.NewReader(mets)
 	_ = r
 
 	// TODO: We need to loop over the archive to find the mets file.
