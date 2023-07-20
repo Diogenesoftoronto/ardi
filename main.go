148c1c073d8c5e2db5fc712afe342860a3ee708c
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-07-20 01:52:54 -0400

    Write to csv when agent Identifier is known

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"encoding/csv"
 	"log"
 	"os"
 	"path/filepath"
@@ -14,7 +14,6 @@ const (
 	ZIP = ".zip"
 	TAR = ".tar"
 	Z7  = ".7z"
-	// TODO: allow the use of xml files directly
 	XML = ".xml"
 )
 
@@ -41,21 +40,42 @@ Only two paths at a time is allowed`)
 	}
 
 	dst, err := os.MkdirTemp(cwd, "METS_Data-")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// TODO: log a fatal error in every case other than the folder not being empty
 	// defer func() {
 	// 	err := os.Remove(dst)
 	// 	if err != nil {
 	// 		log.Fatal(err)
 	// 	}
 	// }()
-	objectPath := etree.MustCompilePath("//premis:object//*")
-	eventPath := etree.MustCompilePath("//premis:event//*") // TODO: add this to a configurable setting so that this can be changed
+	csvF, err := os.Create(filepath.Join(dst, "ComparisonReport.csv"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	w := csv.NewWriter(csvF)
+	defer w.Flush()
+	header := []string{
+		"file", "agent", "outcome", "type", "format"}
+	err = w.Write(header)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// objectPath := etree.MustCompilePath("//premis:object//*")
+	eventPath := etree.MustCompilePath("//premis:event//*")
+	// TODO: add this to a configurable setting so that this can be changed
 	exclude := map[string]bool{
-		"eventIdentifier":             true,
-		"eventDateTime":               true,
-		"linkingAgentIdentifierType":  true,
-		"linkingAgentIdentifier":      true,
-		"linkingAgentIdentifierValue": false,
-		"objectIdentifier":            true,
+		"eventIdentifier":                true,
+		"eventIdentifierType":            true,
+		"eventDateTime":                  true,
+		"linkingAgentIdentifierType":     true,
+		"linkingAgentIdentifier":         true,
+		"linkingAgentIdentifierValue":    false,
+		"objectIdentifier":               true,
+		"fixity":                         true,
+		"objectCharacteristicsExtension": true,
 	}
 
 	paths := os.Args[1:len(os.Args)]
@@ -77,6 +97,7 @@ Only two paths at a time is allowed`)
 			log.Fatal("No mets file found. Is your mets file not all capitalized?")
 		}
 
+		metsFile := filepath.Base(tmpMets.Name())
 		// Create and parse the mets xml file.
 		mets := etree.NewDocument()
 		if err := mets.ReadFromFile(tmpMets.Name()); err != nil {
@@ -87,29 +108,49 @@ Only two paths at a time is allowed`)
 		// Use the correct namespace URI in your SelectElement and SelectElements methods.
 		// These are placeholders and might need to be adjusted according to your XML.
 		// You can find the namespace URI in the XML file, it is the URL specified in the xmlns attribute.
-		objs := root.FindElementsPath(objectPath)
+		// objs := root.FindElementsPath(objectPath)
 		events := root.FindElementsPath(eventPath)
+		var etype string
+		var outcome string
+		var agent string
+		var esize string
+		var format string
+		// var other string
 		// Find all premis:eventType elements
-		for _, el := range objs {
-			if _, excluded := exclude[el.Tag]; !excluded {
-				// fmt.Println(el.Tag)
-				// for _, attr := range el.Attr {
-				// 	fmt.Println(attr.Key, attr.Value)
-				// }
-				// if text := strings.TrimSpace(el.Text()); len(text) > 0 {
-				// 	fmt.Println(i, text, el.Tag)
-				// }
-			}
-		}
+		// for _, el := range objs {
+		// if _, excluded := exclude[el.Tag]; !excluded {
+		// fmt.Print(el.Text(), el.Tag)
+
+		// record := []string{metsFile, agent, el.Text(), etype, format, esize, other}
+		// w.Write(record)
+		// }
+		// }
 		for _, el := range events {
 			if excluded := exclude[el.Tag]; !excluded {
-				// fmt.Println(el.Tag)
-				// for _, attr := range el.Attr {
-				// 	fmt.Println(attr.Key, attr.Value)
-				// }
-				if text := strings.TrimSpace(el.Text()); len(text) > 0 {
-					fmt.Println(i, text, el.Tag)
+				switch el.Tag {
+				case "eventType":
+					etype = el.Text()
+					continue
+				case "linkingAgentIdentifierValue":
+					// TODO: make it configurable which agent identifier is allowed
+					if strings.Contains(el.Text(), "Archivematica") || strings.Contains(el.Text(), "a3m") {
+						agent = el.Text()
+						record := []string{metsFile, agent, outcome, etype, format, esize}
+						w.Write(record)
+					}
+					// case "eventDetail":
+					// other = el.Text()
+					// continue
+				case "eventOutcome":
+					// if strings.Contains(el.Text(), "pass") || strings.Contains(el.Text(), "fail") || el.Text() == "" {
+					outcome = el.Text()
+					continue
+					// }
+				default:
+					// record := []string{metsFile, agent, outcome, etype, format, esize, other}
+					// w.Write(record)
 				}
+
 			}
 		}
 	}
