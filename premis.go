26e514da2ba56f4533ba301c92e3f6b9b099bee7
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-07-20 22:58:05 -0400

    Give structure to the premis records

--- premis.go
+++ premis.go
@@ -1,21 +1,21 @@
 package main
 
-type Premis struct {
+type MetsData struct {
+	Id          int
 	File        string
-	Type        []string
-	Object      []string
-	Outcome     []bool
-	Agent       []string
-	Format      []string
-	EventCount  int
-	SuccesCount int
+	Events      []Event
+	Agent       string // e.g. Archivematica, a3m
+	EventCount  int    // e.g. len(events)
+	SuccesCount int    // e.g. the amount of event.outcome that are positive or pass
 }
 
-// type MetsFile struct {
-// 	Name      string
-// 	PrType    []*etree.Element
-// 	PrObject  []*etree.Element
-// 	PrOutcome []*etree.Element
-// 	PrAgent   []*etree.Element
-// 	PrFormat  []*etree.Element
-// }
+type Event struct {
+	Id            string `json:"id"` //uuid type of field taken from the mets
+	OutcomeDetail string `json:detail`
+	Type          string `json:"type"`    //event type e.g. fixity check, creation
+	ObjectName    string `json`           //premisObjectOrginalName
+	Outcome       bool   `json:"outcome"` //can be empty, but this one is weird e.g. pass, Positive, etc.
+}
+
+// file_1,file_2,events_1,events_2,agent_1,agent_2,eventCount_1,eventCount_2,successCount_1,successCount_2
+// mets-2349.xml,mets-3453.xml,{[1:{"id":"<uuid>","format": "excel", "type": "creation", "outcome": "pass"}]},{[1:{"id":"<uuid>","format": "excel", "type": "creation", "outcome": "pass"}]},Archivematica,a3m,1,1,1,1
